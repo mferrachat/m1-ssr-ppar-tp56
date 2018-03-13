2c92e9ee864187d28c4d4a5fc1eb371ae0383729
mferrachat <melian.ferrachat@gmail.com>
2018-03-13 09:44:24 +0100

    Parce que ça me fait plaisir alors que j'ai rien changé. C'est juste pour avoir la même version que le rendu.

--- src/gamelife.c
+++ src/gamelife.c
@@ -269,10 +269,10 @@ int main(int argc,char *argv[])
 	unsigned int *worldaux;
 
 	// getting started  
-	world1 = initialize_dummy();
+	//world1 = initialize_dummy();
 	//world1 = initialize_random();
 	//world1 = initialize_glider();
-	//world1 = initialize_small_exploder();
+	world1 = initialize_small_exploder();
 	world2 = allocate();
 	print(world1);
 
